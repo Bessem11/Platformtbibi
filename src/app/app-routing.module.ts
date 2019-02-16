684599adbf2a7cd8ed719369148b52573df14ff0
Bessem11 <45499871+Bessem11@users.noreply.github.com>
2019-02-16 14:30:32 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent } from './accueil/accueil.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormulaireComponent } from './auth/formulaire/formulaire.component';
+import { ConnexionComponent } from './auth/connexion/connexion.component';
+import { VideoComponent } from './accueil/video/video.component';
+import { AproposComponent } from './accueil/apropos/apropos.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormulaireComponent,
+    ConnexionComponent,
+    VideoComponent,
+    AproposComponent,
+    InscriptionComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
